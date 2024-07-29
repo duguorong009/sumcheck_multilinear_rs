05d25a6705da045e5534aba874af4b7ec880b648
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-07-29 21:59:04 +0800

    feat: add "binary_to_list" util in "gkr_prover" module

--- src/gkr_prover.rs
+++ src/gkr_prover.rs
@@ -1,1 +1,16 @@
 
+/// Change binary form to list of arguments.
+/// 
+/// `b`: The binary form in little endian encoding. For example, 0b1011 means g(x0=1, x1=1, x2=0, x3=1)
+/// `num_variables`: number of variables
+fn binary_to_list(mut b: usize, num_variables: usize) -> Vec<usize> {
+    let mut lst = vec![0; num_variables];
+    let mut i = 0;
+    while b != 0 {
+        lst[i] = b & 1;
+        b >>= 1;
+        i += 1;
+    }
+    lst
+}
+
