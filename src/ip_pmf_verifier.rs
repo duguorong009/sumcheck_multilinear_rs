810811080ac96effaf42c914a75867bfd31f1a10
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-02 10:48:59 +0800

    feat: add "interpolate" util

--- src/ip_pmf_verifier.rs
+++ src/ip_pmf_verifier.rs
@@ -46,6 +46,37 @@ fn mod_inverse(a: u64, m: u64) -> u64 {
     x.try_into().unwrap()
 }
 
+///Interpolate and evaluate a PMF.
+// Adapted from https://www.geeksforgeeks.org/lagranges-interpolation/
+// :param points: P_0, P_1, P_2, ..., P_m where P is the product of m multilinear polynomials
+// :param r: The point we want to evaluate at. In this scenario, the verifier wants to evaluate p_r.
+// :param p: Field size.
+// :return: P_r
+fn interpolate(points: &[u64], r: u64, p: u64) -> u64 {
+    let mut result = 0;
+    for i in 0..points.len() {
+        let mut term = points[i] % p;
+        for j in 0..points.len() {
+            if j != i {
+                // TODO: `(r - j) % p` & `(i - j) % p` could be not always in the range [0, p-1]
+                let r_min_j = if r >= j as u64 {
+                    r - j as u64
+                } else {
+                    r + p - j as u64
+                };
+                let i_min_j = if i >= j {
+                    (i - j) as u64
+                } else {
+                    i as u64 + p - j as u64
+                };
+                term = (term * (r_min_j % p) * mod_inverse(i_min_j % p, p)) % p;
+            }
+        }
+        result = (result + term) % p;
+    }
+    result
+}
+
 #[test]
 fn test_mod_inverse() {
     assert_eq!(mod_inverse(3, 11), 4);
