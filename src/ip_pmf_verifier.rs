1cfed124f82975ec79e43b2d2e848db1e7a3c90f
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-12 10:24:26 +0800

    fix: update the "IPPMFVerifier" to use the prime field

--- src/ip_pmf_verifier.rs
+++ src/ip_pmf_verifier.rs
@@ -1,81 +1,79 @@
-use rand::{rngs::StdRng, Rng, SeedableRng};
+use halo2curves::ff::PrimeField;
+use rand::{rngs::OsRng, Rng, SeedableRng};
 
 use crate::pmf::PMF;
 
 pub const MAX_ALLOWED_SOUNDNESS_ERROR: f64 = 2e-64;
 
-trait RandomGen {
-    fn get_random_element(&mut self) -> u64 {
+trait RandomGen<F>  where F: PrimeField + Clone {
+    fn get_random_element(&mut self) -> F {
         unimplemented!("get_random_element")
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct TrueRandomGen {
-    rng: StdRng,
-    p: u64,
+    rng: OsRng,
 }
 
 impl TrueRandomGen {
-    fn new(seed: u64, p: u64) -> Self {
-        let rng = rand::rngs::StdRng::seed_from_u64(seed);
-        Self { rng, p }
+    fn new(_seed: u64) -> Self {
+        Self { rng: OsRng }
     }
 }
 
-impl RandomGen for TrueRandomGen {
-    fn get_random_element(&mut self) -> u64 {
-        self.rng.gen_range(0..self.p)
+impl<F> RandomGen<F> for TrueRandomGen where F: PrimeField + Clone {
+    fn get_random_element(&mut self) -> F {
+        F::random(&mut self.rng)
     }
 }
 
 /// An interactive verifier that verifies the sum of the polynomial which is the product of multilinear functions
 #[derive(Debug)]
-pub struct InteractivePMFVerifier {
+pub struct InteractivePMFVerifier<F> where F: PrimeField + Clone {
     checksum_only: bool,
-    p: u64,
-    poly: PMF,
-    asserted_sum: u64,
+    // p: u64,
+    poly: PMF<F>,
+    asserted_sum: F,
     rng: TrueRandomGen,
     pub(crate) active: bool,
     pub(crate) convinced: bool,
-    pub(crate) points: Vec<u64>,
+    pub(crate) points: Vec<F>,
     round: usize,
-    expect: u64,
+    expect: F,
 }
 
-impl InteractivePMFVerifier {
+impl<F> InteractivePMFVerifier<F> where F: PrimeField + Clone {
     pub fn new(
-        poly: PMF,
-        asserted_sum: u64,
+        poly: PMF<F>,
+        asserted_sum: F,
         max_allowed_soundness_err: Option<f64>,
         checksum_only: Option<bool>,
         rng: Option<TrueRandomGen>,
     ) -> Self {
         let poly_num_variables = poly.num_variables;
-        let p = poly.p;
         let seed = rand::thread_rng().gen_range(0..0xFFFFFFFF);
 
-        // check soundness
-        let err = {
-            let deg = poly.num_variables * poly.num_multiplicands();
-            (poly.num_variables * deg) as f64 / poly.p as f64
-        };
-        if err > max_allowed_soundness_err.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR) {
-            panic!("Soundness error {} exceeds maximum allowed soundness error {}. Try to have a prime with size >= ??? bits", err, max_allowed_soundness_err.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR));
-        }
+        // // check soundness
+        // let err = {
+        //     let deg = poly.num_variables * poly.num_multiplicands();
+        //     (poly.num_variables * deg) as f64 / F::MODULUS as f64
+        // };
+        // if err > max_allowed_soundness_err.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR) {
+        //     panic!("Soundness error {} exceeds maximum allowed soundness error {}. Try to have a prime with size >= ??? bits", err, max_allowed_soundness_err.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR));
+        // }
 
         // some edge cases: if univariate or constant: no need to be interactive
         if poly.num_variables == 0 {
-            if (asserted_sum - poly.eval(&[])) % poly.p == 0 {
+            if (asserted_sum - poly.eval(&[])) == F::ZERO {
                 todo!("implement `_convince_and_close` & return")
             } else {
                 todo!("implement `_reject_and_close` & return")
             }
         }
         if poly.num_variables == 1 {
-            let result = (poly.eval(&[0]) + poly.eval(&[1])) % poly.p;
-            if (asserted_sum - result) % poly.p == 0 {
+            let result = poly.eval(&[F::ZERO]) + poly.eval(&[F::ONE]);
+            if asserted_sum - result == F::ZERO {
                 todo!("implement `_convince_and_close` & return")
             } else {
                 todo!("implement `_reject_and_close` & return")
@@ -84,25 +82,25 @@ impl InteractivePMFVerifier {
 
         Self {
             checksum_only: checksum_only.unwrap_or(false),
-            p,
             poly,
-            asserted_sum: asserted_sum % p,
-            rng: rng.unwrap_or_else(|| TrueRandomGen::new(seed, p)),
+            asserted_sum: asserted_sum,
+            rng: rng.unwrap_or_else(|| TrueRandomGen::new(seed)),
             active: true,
             convinced: false,
-            points: vec![0; poly_num_variables],
+            points: vec![F::ZERO; poly_num_variables],
             round: 0,
             expect: asserted_sum,
         }
     }
 
-    fn random_r(&mut self) -> u64 {
+    fn random_r(&mut self) -> F {
         self.rng.get_random_element()
     }
 
     fn soundness_error(&self) -> f64 {
-        let deg = self.poly.num_variables * self.poly.num_multiplicands();
-        (self.poly.num_variables * deg) as f64 / self.p as f64
+        // let deg = self.poly.num_variables * self.poly.num_multiplicands();
+        // (self.poly.num_variables * deg) as f64 / self.p as f64
+        unimplemented!("soundness_error")
     }
 
     /// The minimum size of prime required to meet the soundness error constraint.
@@ -115,7 +113,7 @@ impl InteractivePMFVerifier {
     /// Send this verifier the univariate polynomial P(x). P(x) has degree at most the number of multiplicands.
     /// :param msgs: [P(0), P(1), ..., P(m)] where m is the number of multiplicands
     /// :return: accepted, r
-    pub fn talk(&mut self, msgs: &[u64]) -> (bool, u64) {
+    pub fn talk(&mut self, msgs: &[F]) -> (bool, F) {
         if !self.active {
             panic!("Unable to prove: the protocol is not active.");
         }
@@ -127,16 +125,16 @@ impl InteractivePMFVerifier {
             );
         }
 
-        let p0 = msgs[0] % self.p;
-        let p1 = msgs[1] % self.p;
-        if (p0 + p1) % self.p != self.expect % self.p {
+        let p0 = msgs[0];
+        let p1 = msgs[1];
+        if p0 + p1 != self.expect {
             self._reject_and_close();
-            return (false, 0);
+            return (false, F::ZERO);
         }
 
         // pick r at random
         let r = self.random_r();
-        let pr = interpolate(msgs, r, self.p);
+        let pr = interpolate(msgs, r);
 
         self.expect = pr;
         self.points[self.round] = r;
@@ -173,7 +171,7 @@ impl InteractivePMFVerifier {
     ///  - the expected evaluation at this point
     ///
     ///     :return: (point: Vec<u64>, expected: u64)
-    pub fn subclaim(&self) -> (Vec<u64>, u64) {
+    pub fn subclaim(&self) -> (Vec<F>, F) {
         if !self.convinced {
             panic!("The verifier is not convinced, and cannot make a sub claim.");
         }
@@ -195,35 +193,35 @@ impl InteractivePMFVerifier {
     // TODO: Should implement the "_repr_html_" method ???
 }
 
-// modular inverse (https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/)
-//     :param a: a
-//     :param m: prime
-//     :return: a^-1 mod p
-fn mod_inverse(a: u64, m: u64) -> u64 {
-    let m0: i128 = m.into();
-    let mut y: i128 = 0;
-    let mut x: i128 = 1;
+// // modular inverse (https://www.geeksforgeeks.org/multiplicative-inverse-under-modulo-m/)
+// //     :param a: a
+// //     :param m: prime
+// //     :return: a^-1 mod p
+// fn mod_inverse(a: u64, m: u64) -> u64 {
+//     let m0: i128 = m.into();
+//     let mut y: i128 = 0;
+//     let mut x: i128 = 1;
 
-    if m == 1 {
-        return 0;
-    }
+//     if m == 1 {
+//         return 0;
+//     }
 
-    let mut m: i128 = m.into();
-    let mut a: i128 = a.into();
-    while a > 1 {
-        let q = a / m;
-        let mut t: i128 = m;
-        m = a % m;
-        a = t;
-        t = y;
-        y = x - q * y;
-        x = t;
-    }
-    if x < 0 {
-        x += m0;
-    }
-    x.try_into().unwrap()
-}
+//     let mut m: i128 = m.into();
+//     let mut a: i128 = a.into();
+//     while a > 1 {
+//         let q = a / m;
+//         let mut t: i128 = m;
+//         m = a % m;
+//         a = t;
+//         t = y;
+//         y = x - q * y;
+//         x = t;
+//     }
+//     if x < 0 {
+//         x += m0;
+//     }
+//     x.try_into().unwrap()
+// }
 
 ///Interpolate and evaluate a PMF.
 // Adapted from https://www.geeksforgeeks.org/lagranges-interpolation/
@@ -231,32 +229,16 @@ fn mod_inverse(a: u64, m: u64) -> u64 {
 // :param r: The point we want to evaluate at. In this scenario, the verifier wants to evaluate p_r.
 // :param p: Field size.
 // :return: P_r
-fn interpolate(points: &[u64], r: u64, p: u64) -> u64 {
-    let mut result = 0;
+fn interpolate<F: PrimeField + Clone>(points: &[F], r: F) -> F {
+    let mut result = F::ZERO;
     for i in 0..points.len() {
-        let mut term = points[i] % p;
+        let mut term = points[i];
         for j in 0..points.len() {
             if j != i {
-                // TODO: `(r - j) % p` & `(i - j) % p` could be not always in the range [0, p-1]
-                let r_min_j = if r >= j as u64 {
-                    r - j as u64
-                } else {
-                    r + p - j as u64
-                };
-                let i_min_j = if i >= j {
-                    (i - j) as u64
-                } else {
-                    i as u64 + p - j as u64
-                };
-                term = (term * (r_min_j % p) * mod_inverse(i_min_j % p, p)) % p;
+                term *= (r - F::from_u128(j as u128)) * (F::from_u128(i as u128) - F::from_u128(j as u128)).invert().unwrap();
             }
         }
-        result = (result + term) % p;
+        result += term;
     }
     result
-}
-
-#[test]
-fn test_mod_inverse() {
-    assert_eq!(mod_inverse(3, 11), 4);
 }
