98efe8f8157e67bac28a723ef03262599e5d59c5
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-22 21:32:17 +0800

    feat: add impls for "PseudoRandomGen"

--- src/fs_pmf_verifier.rs
+++ src/fs_pmf_verifier.rs
@@ -10,3 +10,13 @@ where
     pub(crate) poly: PMF<F>,
     pub(crate) message: Vec<Vec<F>>,
 }
+
+impl<F> PseudoRandomGen<F> where F: PrimeField + Clone {
+    pub fn new(poly: PMF<F>, message: Vec<Vec<F>>) -> PseudoRandomGen<F> {
+        PseudoRandomGen { poly, message }
+    }
+
+    pub fn get_random_element(&self) -> F {
+        random_element(&self.poly, &self.message)
+    }
+}
