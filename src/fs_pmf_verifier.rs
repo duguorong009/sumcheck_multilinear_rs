e18b18969b0e77919b6d6e2f732981464b079546
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-23 08:44:28 +0800

    feat: implement the "verify_proof" utility

--- src/fs_pmf_verifier.rs
+++ src/fs_pmf_verifier.rs
@@ -43,3 +43,15 @@ impl<F> Proof<F> where F: PrimeField + Clone {
         Proof { prover_message }
     }
 }
+
+fn verify_proof<F>(theorem: &Theorem<F>, proof: &Proof<F>, max_allowed_soundness_error: Option<f64>) -> bool where F: PrimeField + Clone {
+    let max_allowed_soundness_error = max_allowed_soundness_error.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR);
+    let gen = PseudoRandomGen::new(theorem.poly.clone());
+    let v = InteractivePMFVerifier::new(theorem.poly.clone(), theorem.asserted_sum.clone(), Some(max_allowed_soundness_error), None, Some(gen));
+
+    for msg in proof.prover_message.iter() {
+        gen.message.push(msg.clone());
+        v.talk(&msg);
+    }
+    v.convinced
+}
