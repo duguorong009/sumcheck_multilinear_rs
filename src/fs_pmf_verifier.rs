3a5c0c48a7db70ae6b4298fff322419500aa427e
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-09-24 11:51:06 +0800

    fix: correct the "verify_proof" utility in "fs_pmf_verifier"

--- src/fs_pmf_verifier.rs
+++ src/fs_pmf_verifier.rs
@@ -1,8 +1,24 @@
+use std::{cell::RefCell, rc::Rc};
+
 use halo2curves::ff::PrimeField;
 
-use crate::pmf::PMF;
+use crate::{ip_pmf_verifier::{InteractivePMFVerifier, RandomGen}, pmf::PMF};
 
-#[derive(Debug)]
+const MAX_ALLOWED_SOUNDNESS_ERROR: f64 = 2e-64;
+
+/// Sample a random element in the field using hash function which takes the polynomial and prover message as input.
+/// 
+///  :param poly: The polynomial
+///  :param proverMessage: List of messages [P(0), P(1), P(2), ..., P(m)]
+///  :return:
+fn random_element<F>(poly: &PMF<F>, prover_message: &Vec<Vec<F>>) -> F where F: PrimeField + Clone {
+    // let hash_size = (F::NUM_BITS + 7) / 8;
+    // let byte_length = hash_size;
+
+    todo!()
+}
+
+#[derive(Debug, Clone)]
 pub struct PseudoRandomGen<F>
 where
     F: PrimeField + Clone,
@@ -15,8 +31,10 @@ impl<F> PseudoRandomGen<F> where F: PrimeField + Clone {
     pub fn new(poly: PMF<F>) -> PseudoRandomGen<F> {
         PseudoRandomGen { poly, message: vec![] }
     }
+}
 
-    pub fn get_random_element(&self) -> F {
+impl<F> RandomGen<F> for PseudoRandomGen<F> where F: PrimeField + Clone {
+    fn get_random_element(&mut self) -> F {
         random_element(&self.poly, &self.message)
     }
 }
@@ -46,8 +64,8 @@ impl<F> Proof<F> where F: PrimeField + Clone {
 
 fn verify_proof<F>(theorem: &Theorem<F>, proof: &Proof<F>, max_allowed_soundness_error: Option<f64>) -> bool where F: PrimeField + Clone {
     let max_allowed_soundness_error = max_allowed_soundness_error.unwrap_or(MAX_ALLOWED_SOUNDNESS_ERROR);
-    let gen = PseudoRandomGen::new(theorem.poly.clone());
-    let v = InteractivePMFVerifier::new(theorem.poly.clone(), theorem.asserted_sum.clone(), Some(max_allowed_soundness_error), None, Some(gen));
+    let mut gen = PseudoRandomGen::new(theorem.poly.clone());
+    let mut v = InteractivePMFVerifier::new(theorem.poly.clone(), theorem.asserted_sum.clone(), Some(max_allowed_soundness_error), None, Some(Rc::new(RefCell::new(gen.clone()))));
 
     for msg in proof.prover_message.iter() {
         gen.message.push(msg.clone());
